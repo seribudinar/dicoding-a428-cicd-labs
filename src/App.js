c5f6e57cf4ba934664c009628eafc163c43bf8ca
Edhy Khalifatullah <edhy.khal@gmail.com>
2023-08-04 23:16:35 +0700

    edit app.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <p>success render</p>
       </div>
     );
   }
